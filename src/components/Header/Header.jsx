083a32a3f31a853442a65900ee4b08115de07865
unknown <sujoydaspc2023@gmail.com>
2025-01-19 23:44:27 +0600

    adding style

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,6 +2,8 @@ import { NavLink } from "react-router-dom"
 import { IoMdMenu } from "react-icons/io";
 import { IoClose } from "react-icons/io5";
 const Header = () => {
+
+  
   return (
     <div className="fixed w-full  font-open-sans border bg-orange-200 bg-opacity-55 text-black">
       <div className=" max-w-screen-2xl mx-auto ">
@@ -24,21 +26,21 @@ const Header = () => {
             </div>
           </div>
 
-          <div className="flex flex-col gap-2 border p-2 text-xs rounded-lg bg-white">
+          <div className="text-5xl bg-white rounded-md">
+            <IoMdMenu />
+            <IoClose />
+          </div>
+
+          <div className="flex flex-col absolute top-24 right-3 gap-2 border p-3 text-[18px] rounded-lg bg-white">
             <div className="flex flex-col gap-2">
               <NavLink className="hover:text-[#ffc684] px-2 py-1 rounded-lg active:scale-95 transition-all" to={"/"}>Home</NavLink>
               <NavLink className="hover:text-[#ffc684] px-2 py-1 rounded-lg active:scale-95 transition-all" to={"/aboutus"}>About Us</NavLink>
               <NavLink className="hover:text-[#ffc684] px-2 py-1 rounded-lg active:scale-95 transition-all" to={"/tourpackages"}>Tour Packages</NavLink>
               <NavLink className="hover:text-[#ffc684] px-2 py-1 rounded-lg active:scale-95 transition-all" to={"/contactus"}>Contact Us</NavLink>
             </div>
-
-            <div>
-              <IoMdMenu />
-              <IoClose />
-            </div>
             <div className="flex flex-col gap-2 text-center">
-              <NavLink className="hover:bg-[#FA8B02]  px-4 py-2 text-black  active:scale-95 rounded-full  border-2 hover:border-transparent border-orange-400  transition-all" to={"/login"}>Login</NavLink>
-              <NavLink className="bg-[#FA8B02] px-4 py-2 active:scale-95 rounded-full hover:bg-transparent border-2 hover:text-orange-400 border-transparent hover:border-orange-400 transition-all" to={"/signup"}>Sign Up</NavLink>
+              <NavLink className="hover:bg-[#FA8B02]  px-2 py-1 text-black  active:scale-95 rounded-full  border-2 hover:border-transparent border-orange-400  transition-all" to={"/login"}>Login</NavLink>
+              <NavLink className="bg-[#FA8B02] px-2 py-1 active:scale-95 rounded-full hover:bg-transparent border-2 hover:text-orange-400 border-transparent hover:border-orange-400 transition-all" to={"/signup"}>Sign Up</NavLink>
             </div>
           </div>
 
