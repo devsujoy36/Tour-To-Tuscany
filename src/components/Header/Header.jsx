75900317d922e141b55d1144de54e2646cc3884a
unknown <sujoydaspc2023@gmail.com>
2025-01-19 23:52:33 +0600

    menu close toggler

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,19 @@
 import { NavLink } from "react-router-dom"
 import { IoMdMenu } from "react-icons/io";
 import { IoClose } from "react-icons/io5";
+import { useState } from "react";
 const Header = () => {
-
-  
+  const [navSmToggle, setNavSmToggle] = useState(true)
+  const NavHandler = () => {
+      if (navSmToggle) {
+       setNavSmToggle(!navSmToggle) 
+      }
+      setNavSmToggle(true)
+  }
   return (
-    <div className="fixed w-full  font-open-sans border bg-orange-200 bg-opacity-55 text-black">
+    <div className="fixed w-full font-open-sans border bg-orange-200 bg-opacity-55 text-black">
       <div className=" max-w-screen-2xl mx-auto ">
-        <nav className=" flex justify-between  items-center text-xl font-medium py-2 px-10">
+        <nav className=" flex justify-between  items-center text-xl font-medium py-2 lg:px-10 px-5">
 
           <div>
             <img className="w-20" src="https://i.ibb.co.com/dQ3twTX/navlogo.png" alt="navLogo" />
@@ -26,12 +32,12 @@ const Header = () => {
             </div>
           </div>
 
-          <div className="text-5xl bg-white rounded-md">
-            <IoMdMenu />
-            <IoClose />
+          <div onClick={NavHandler} className="text-5xl bg-white rounded-md p-1  active:scale-95 transition-all active:rotate-180">
+            {navSmToggle ? <IoMdMenu /> : <IoClose />}
+
           </div>
 
-          <div className="flex flex-col absolute top-24 right-3 gap-2 border p-3 text-[18px] rounded-lg bg-white">
+          <div className="flex flex-col absolute mx-2 mt-3 top-24 right-3 gap-2 border p-3 text-[18px] rounded-lg bg-white">
             <div className="flex flex-col gap-2">
               <NavLink className="hover:text-[#ffc684] px-2 py-1 rounded-lg active:scale-95 transition-all" to={"/"}>Home</NavLink>
               <NavLink className="hover:text-[#ffc684] px-2 py-1 rounded-lg active:scale-95 transition-all" to={"/aboutus"}>About Us</NavLink>
