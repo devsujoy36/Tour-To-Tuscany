495f601042a879b1298b73c62bfc01e8222c41e7
unknown <sujoydaspc2023@gmail.com>
2025-01-22 04:23:48 +0600

    update profile

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -7,6 +7,8 @@ const Profile = () => {
   const [profileError, setProfileError] = useState()
   const [success, setSuccess] = useState()
 
+  const [updateForm, setUpdateForm] = useState(false)
+
   const hangleSubmit = (e) => {
     e.preventDefault()
 
@@ -14,20 +16,16 @@ const Profile = () => {
 
 
   return (
-    <div className='min-h-[100vh] pt-32 lg:text-2xl font-baloo-2'>
+    <div className='min-h-[100vh] pt-32 pb-10 lg:text-2xl font-baloo-2'>
       <div className="max-w-screen-2xl md:mx-auto mx-10">
         <h1 className="text-5xl  font-medium mb-5">Profile</h1>
 
         <div className="flex items-start gap-10 flex-col md:flex-row">
-
-
           <div className="grid gap-8 lg:w-2/3">
-
             <figure className="border shadow-xl p-5 h-30 flex items-center gap-3  rounded-full ">
               <img className="w-28 h-28 border rounded-full" src={user?.photoURL} alt="" />
               <h1 className="font-medium text-3xl">{user?.displayName}</h1>
             </figure>
-
             <div className="md:mx-10 grid gap-3">
               <h1><span className="font-medium">Email: </span>{user?.email}</h1>
               <h1 className="flex items-center gap-1">
@@ -46,33 +44,49 @@ const Profile = () => {
               <h1><span className="font-medium">Last Sign in Time: </span>{user?.metadata.lastSignInTime}</h1>
 
             </div>
-
           </div>
 
-          <div className="lg:w-1/3 border p-10 rounded-xl">
-            <form onSubmit={hangleSubmit}>
-              <h1 className="text-4xl font-bold mb-5">Update Profile</h1>
+          <div className="lg:w-1/3">
 
-              <div className="flex flex-col gap-1 text-gray-500 text-lg">
-                <label htmlFor="" className="text-xl font-medium">Name and Surname</label>
-                <input type="text" name="name" placeholder="Enter your name and surname" className="px-4 py-3 rounded-md border-2" required />
-                <label htmlFor="" className="text-xl font-medium">Email Adress</label>
-                <input type="email" name="email" placeholder="Enter your email adress" className="px-4 py-3 rounded-md border-2" required />
-                {profileError && <p className="text-xs text-red-500">{profileError}</p>}
-                {success && <p className="text-xs text-emerald-500">{success}</p>}
-                <label htmlFor="" className="text-xl font-medium">Password</label>
-                <input type="password" name="password" placeholder="Enter your Password" className="px-4 py-3 rounded-md border-2" required />
-                <div className="flex mt-2 justify-start items-center gap-1">
-                  <input id="checkbox" type="checkbox" required />
-                  <label htmlFor="checkbox">I agree with <a href="" className="text-orange-400">Terms</a>  and <a href="" className="text-orange-400">Privacy</a></label>
+            {
+              updateForm
+                ?
+                <div onClick={() => setUpdateForm(!updateForm)} className="flex md:justify-start md:mx-10 justify-center items-center">
+                  <button className="border-2 text-white bg-orange-500 px-4 py-2 rounded-full hover:px-8 active:scale-95 transition-all">Click for Update Your Profile</button>
                 </div>
-              </div>
-              <div className="text-center">
+                :
+                <div className=" border lg:p-10 p-5 rounded-xl">
+                  <form onSubmit={hangleSubmit}>
+                    <h1 className="md:text-4xl text-2xl font-bold mb-5">Update Profile</h1>
 
-                <input type="submit" value={"Sign Up"} className="border-2 w-full rounded-full cursor-pointer text-white bg-orange-500 py-2 text-xl mt-3 active:scale-95 hover:bg-transparent hover:text-orange-500 transition-all" />
-              </div>
-            </form>
+                    <div className="flex flex-col gap-1 text-gray-500 text-lg">
+                      <label htmlFor="" className="text-xl font-medium">Profile Photo</label>
+                      <input type="file" name="profileImg" className="px-4 py-3 lg:w-full w-72 rounded-md border-2 " required />
+
+                      <label htmlFor="" className="text-xl font-medium">Name and Surname</label>
+                      <input type="text" name="name" placeholder="Enter your name and surname" className="px-4 py-3 rounded-md border-2 " required />
+
+                      <label htmlFor="" className="text-xl font-medium">Phone Number</label>
+                      <input type="number" name="phone" placeholder="Enter your phone number" className="px-4 py-3 rounded-md border-2 " required />
+
+
+                      <div className="flex mt-2 justify-start items-center gap-1">
+                        <input id="checkbox" type="checkbox" required />
+                        <label htmlFor="checkbox">I agree with <a href="" className="text-orange-400">Terms</a>  and <a href="" className="text-orange-400">Privacy</a></label>
+                      </div>
+                    </div>
+                    <div className="text-center">
+
+                      <input type="submit" value={"Update"} className="border-2 w-full rounded-full cursor-pointer text-white bg-orange-500 py-2 text-xl mt-3 active:scale-95 hover:bg-transparent hover:text-orange-500 transition-all" />
+                    </div>
+                    {profileError && <p className="text-xs text-red-500">{profileError}</p>}
+                    {success && <p className="text-xs text-emerald-500">{success}</p>}
+                  </form>
+                </div>
+            }
           </div>
+
+
 
 
         </div>
